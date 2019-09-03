54c634dbc3197c790f068a9377f56fc85ba04525
TimBoudewijns <51700403+TimBoudewijns@users.noreply.github.com>
2019-09-03 15:38:54 +0200

    added build

---
+++ build/precache-manifest.8dfa62ae08e791ac3e3d8b8543d4a99e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dd2b2654785af07691f23e435c201d3",
+    "url": "/Robo-Friends/index.html"
+  },
+  {
+    "revision": "79c6f1758e88c906dcb6",
+    "url": "/Robo-Friends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "8205e6b4be5ce07c45bc",
+    "url": "/Robo-Friends/static/css/main.b5b3bb33.chunk.css"
+  },
+  {
+    "revision": "79c6f1758e88c906dcb6",
+    "url": "/Robo-Friends/static/js/2.80c069d3.chunk.js"
+  },
+  {
+    "revision": "8205e6b4be5ce07c45bc",
+    "url": "/Robo-Friends/static/js/main.d3a16643.chunk.js"
+  },
+  {
+    "revision": "e494226c771cd840f2f8",
+    "url": "/Robo-Friends/static/js/runtime~main.5c4af472.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robo-Friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
